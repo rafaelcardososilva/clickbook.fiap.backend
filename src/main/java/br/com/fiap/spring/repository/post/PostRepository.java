80ac5c82ea47a53555bf48155154b15825f9cea8
Ricardo <rlribeiro@hotmail.com.br>
2022-04-22 11:09:14 -0300

    fix queery

--- src/main/java/br/com/fiap/spring/repository/post/PostRepository.java
+++ src/main/java/br/com/fiap/spring/repository/post/PostRepository.java
@@ -8,7 +8,12 @@ import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Integer> {
 
-    @Query(value = "SELECT * FROM TB_POST", nativeQuery=true)
+//    @Query(value = "SELECT * FROM TB_POST", nativeQuery=true)
+//    List<Post> buscarPosts();
+
+    @Query(value = "insert into tb_post(id, categoria, fotourl, numlikes, texto) values(2, 'culinária', 'url', 0, 'Foto de um ovo de pascoa')", nativeQuery=true)
     List<Post> buscarPosts();
+
+
 
 }
