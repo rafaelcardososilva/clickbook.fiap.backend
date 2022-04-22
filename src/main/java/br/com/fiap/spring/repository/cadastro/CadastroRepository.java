4f152157d92251ceed4a69114744c4a33907fa8c
Ricardo <rlribeiro@hotmail.com.br>
2022-04-22 16:24:29 -0300

    fix

--- src/main/java/br/com/fiap/spring/repository/cadastro/CadastroRepository.java
+++ src/main/java/br/com/fiap/spring/repository/cadastro/CadastroRepository.java
@@ -6,8 +6,8 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface CadastroRepository extends JpaRepository<Cadastro, Integer> {
 
-    String queryString = "SELECT nome, senha FROM TBCADASTRO WHERE NOME = '%:nome% and SENHA = '%:senha%";
-
+    String queryString = "SELECT nome, senha FROM TBCADASTRO WHERE NOME = %:nome% and SENHA = %:senha%";
+    
     @Query(value = queryString, nativeQuery = true)
     Cadastro buscarLogin(String nome, String senha);
 
